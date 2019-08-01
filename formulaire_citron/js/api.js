bb235384518ba7b26e748d889dd631540d7e2e45
karima <karima.lafria@gmail.com>
2019-08-01 17:59:46 +0200

    modif api

--- formulaire_citron/js/api.js
+++ formulaire_citron/js/api.js
@@ -1,53 +1,69 @@
 $(document).ready(function() {
 
+    $.ajax({
+        type: 'GET',
+        url: "http://127.0.0.1:8000/categorie/api",
+    }).done(function(res) {
+        console.log(res);
 
+        /**
+         * Affichage première question
+         */
+        let startFirstQuestion = '<div class="enfant">' +
+            '<form id="1">' +
+            '<legend>' +
+            'De quoi avez vous besoin ?' +
+            '</legend>';
+        
+        let suiteFirstQuestion = '';
+
+        $.each(res, function(category, value) {
+            console.log(category);
+            console.log(value);
+
+            let categoryHtml = '<input data-id="' + category + '" value="' + category + '" type="checkbox" name="choiceCategory" id="' + category +'">'+
+                                '<label for="' + category +'">'+category+'</label>';
+
+            suiteFirstQuestion += categoryHtml;
+        });
+
+        let endFirstQuestion = '<input type="submit" value="envoyer"></form></div>';
+
+        let fullFirstQuestion = startFirstQuestion + suiteFirstQuestion + endFirstQuestion;
+
+        /**
+         * Affichage questions conditionnées
+         */
+
+        let questionsSuivantes = '';
+        $.each(res, function (category, value) {
+            console.log(category);
+            console.log(value);
+
+            let categoryHtml = '<div class="enfant">'+
+                                '<form id="'+category+'">'+
+                                '<legend>'+
+                                value.question +
+                                '</legend>';
+
+            let reponses = '';
+                
+            $.each(value.reponses, function (key, reponse) {
+
+                let reponseHtml = '<input data-id="4" value="categorie_id_5" type="checkbox" name="reponse_3" id="id_3_1">' +
+                    '<label for="id_3_1">'+reponse+'</label>';
+
+                reponses += reponseHtml;
+            });
+
+
+            let finCategoryHtml = '<input type="submit" value="envoyer">'+
+                                    '</form>'+
+                                    '</div>';
+
+            questionsSuivantes = categoryHtml + reponses + finCategoryHtml;
+        })
     
-    $('form').on('submit', function(){
-
-        const reponse = $(this).children('input[type="checkbox"]').val();
-        const nextSlide = $(this).children('input[type="checkbox"]').attr('data-id');
-        const formId = $(this).attr('id');
-        const monTableau = [];
-        monTableau['question' + formId] = reponse;
-        e.preventDefault();
-        console.log(monTableau);
-        $(this).parent().fadeOut();
-        $('#' + nextSlide).parent().fadeIn();
-
-        if(formId == $('form').lenght){
-
-        $.ajax({
-    
-        type: 'POST',
-        url: "reponse/result",
-        dataType: 'json',
-        data: {
-            'reponse': $(this).children('input[type="checkbox"]').val(),
-            'monTableau': ['question' + formId] = reponse,
-        },
-
-        success : function(){
-
-
-        }
-
+        $('#questions').html(fullFirstQuestion + questionsSuivantes);
     })
-            .done(function (result) {
-                console.log(result);
-                const nextSlide = $(this).children('input[type="checkbox"]').attr('data-id');
-                console.log(result)
-            })
-    })
-   
 })
-
-}
-/* .ajax 
-
-type
-url
-dataType
-data: {
-
-}
-
