824c7957ff198bd648ffa600eb2fb89c0f2c6c49
Laure-Marie <lauremarie.torre@gmail.com>
2019-08-01 12:45:11 +0200

    page produit

--- PRODUITS/js/scripts_form.js
+++ PRODUITS/js/scripts_form.js
@@ -1,11 +1,16 @@
 $(document).ready(function(){
 
+    // FOND PROGRESSIF
+
+    
+
     function EasyPeasyParallax() {
         let scrollPos = $(document).scrollTop();
         let targetOpacity = 0;
-
+        console.log('scroll position:'+(scrollPos*100)/10);
+        
         let userHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-        scrollPos > 0.20*userHeight ? ( targetOpacity = '0.'+ (scrollPos*100)/10 ) : 1;
+        scrollPos > 0.20*userHeight ? ( targetOpacity = ((scrollPos*100)/10 ) / 10000) : 1;
 
         console.log(targetOpacity)
         $('#produit').css({
@@ -21,14 +26,14 @@ $(document).ready(function(){
     });
 
 
-
+    // BOUTON QUANTITE
     var quantity=0;
         $('.quantity-right-plus').click(function(e){
             
             // Stop acting like a button
             e.preventDefault();
             // Get the field name
-            var quantity = parseInt($('#quantity').val());
+            let quantity = parseInt($('#quantity').val());
             
             // If is not undefined
                 
@@ -43,7 +48,7 @@ $(document).ready(function(){
             // Stop acting like a button
             e.preventDefault();
             // Get the field name
-            var quantity = parseInt($('#quantity').val());
+            let quantity = parseInt($('#quantity').val());
             
             // If is not undefined
             
@@ -51,5 +56,29 @@ $(document).ready(function(){
                 if(quantity>0){
                 $('#quantity').val(quantity - 1);
                 }
-    });       
-});
+    });  
+    
+    // MENU DESCRIPTION
+
+    $('.cara_produits_1').css('display', 'none');
+    $('.cara_butn_1').on('click',function(){
+        $('.cara_produits_1').slideToggle(200);
+    })
+    $('.cara_produits_2').css('display', 'none');
+    $('.cara_butn_2').on('click',function(){
+        $('.cara_produits_2').slideToggle(200);
+    })
+
+    $('.cara_produits_3').css('display', 'none');
+    $('.cara_butn_3').on('click',function(){
+        $('.cara_produits_3').slideToggle(200);
+    })
+
+    // SLIDER
+    $('.autoplay').slick({
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 2000,
+        });
+    });
